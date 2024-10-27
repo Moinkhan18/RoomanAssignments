d996c98e4d6348114b54d4b87446f6abbc18b468
Moinkhan18 <moink63601@gmail.com>
2024-10-27 18:36:15 +0530

    Delete feature.py

--- feature.py
+++
@@ -1,9 +0,0 @@
-from termcolor import colored
-
-text = "Hello World"
-
-colors = ['red', 'yellow', 'green', 'cyan', 'blue', 'magenta']
-
-for i in range(len(text)):
-    print(colored(text[i], colors[i % len(colors)]), end="")
-print()
